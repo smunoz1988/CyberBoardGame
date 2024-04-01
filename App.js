3822df82d1c9f6e892d5c357824ad7a8b7f356f4
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-04-01 17:29:31 -0500

    setup app

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.tittle}>CYBER BOARD GAME</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tittle: {
+    color: 'white',
+    fontSize: 30,
+  },
 });
+
+export default App;
