cca02ea8409aa9edfd0b96166421564f2a540936
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-04-08 17:47:09 -0500

    add mercenaries data

--- Screens/HeroesScreen.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const HeroesScreen = () => {
-  return (
-    <div>HeroesScreen</div>
-  )
-}
---
+++ Screens/MercenaryScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MercenaryScreen = () => {
+  return (
+    <div>MercenaryScreen</div>
+  )
+}
+
+export default MercenaryScreen;
