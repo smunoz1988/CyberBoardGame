2308a8f95483ed53575077c4dae164bc2e3336f6
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-04-09 18:28:15 -0500

    update font type on global app

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/HomeScreen';
 import MissionScreen from './Screens/MissionScreen';
 import MercenaryScreen from './Screens/MercenaryScreen';
+import { useFonts, Orbitron_400Regular, Orbitron_900Black } from '@expo-google-fonts/orbitron';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  let [fontsLoaded] = useFonts({
+    Orbitron_400Regular,
+    Orbitron_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Loading...</Text></View>;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- Components/MercenaryItem.js
+++ Components/MercenaryItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+
 const MercenaryItem = ({ mercenary, onMercenarySelect }) => {
+
   return (
     <TouchableOpacity style={styles.heroCard} onPress={() => onMercenarySelect(mercenary)}>
         <View>
-            <Text>{mercenary.name}</Text>
+            <Text style={styles.mercenaryName}>{mercenary.name}</Text>
             <Text>Health: {mercenary.health}</Text>
             <Text>Styles:</Text>
             {mercenary.styles.map((style, index) => (
@@ -27,6 +29,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     margin: 10,
     padding: 10,
+  },
+  mercenaryName: {
+    fontFamily: 'Orbitron_900Black',
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
 
