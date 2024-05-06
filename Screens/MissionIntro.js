3e7f3a091274eff5a77327019a65a0cbf5aafe14
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-05-06 14:09:53 -0500

    update counter method on missionIntro

--- Screens/MissionIntro.js
+++ Screens/MissionIntro.js
@@ -1,15 +1,23 @@
 import { ScrollView, Text } from 'react-native';
 import EnemyInitiative from "../Components/EnemyInitiative";
 
-const MissionIntro = ({ route, navigation }) => {
-  const { playerNames, mission } = route.params;
+const MissionIntro = ({ route }) => {
+  const { selectedMercenaries, mission } = route.params;
   const enemies = mission.enemies;
-  console.log('MissionIntro', mission);
+  const mercenaries = selectedMercenaries;
+  let counter = 0;
+
+  console.log('mission', mission);
+  console.log('enemies', enemies);
+  console.log('mercenaries', mercenaries);
+
   return (
     <ScrollView>
       {enemies.map((enemy, index) => (
+        counter = 0,
         Array.from({ length: enemy.quantity }).map((_, i) => (
-          <EnemyInitiative key={`${index}-${i}`} enemy={enemy} />
+          counter+=1,
+          <EnemyInitiative key={`${index}-${i}`} id={counter} enemy={enemy} />
         ))
       ))}
       <Text>Objective:</Text>
