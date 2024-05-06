b642a4f937bd2514f574e8da78950138652e8078
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-05-06 13:18:47 -0500

    include enemyInitiatives on mission intro

--- Screens/MissionIntro.js
+++ Screens/MissionIntro.js
@@ -1,8 +1,19 @@
-import { Text } from "react-native";
+import { ScrollView, Text } from 'react-native';
+import EnemyInitiative from "../Components/EnemyInitiative";
 
-const MissionIntro = () => {
+const MissionIntro = ({ route, navigation }) => {
+  const { playerNames, mission } = route.params;
+  const enemies = mission.enemies;
+  console.log('MissionIntro', mission);
   return (
-    <Text>mission details here</Text>
+    <ScrollView>
+      {enemies.map((enemy, index) => (
+        Array.from({ length: enemy.quantity }).map((_, i) => (
+          <EnemyInitiative key={`${index}-${i}`} enemy={enemy} />
+        ))
+      ))}
+      <Text>Objective:</Text>
+    </ScrollView>
   )
 }
 
