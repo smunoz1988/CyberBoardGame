fa86ffc62d7f06113db2fa551cf8d7fae376da70
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-05-07 11:21:15 -0500

    create logic to manege enemies and mercenaries initiative

--- Screens/MissionIntro.js
+++ Screens/MissionIntro.js
@@ -4,25 +4,38 @@ import EnemyInitiative from "../Components/EnemyInitiative";
 const MissionIntro = ({ route }) => {
   const { selectedMercenaries, mission } = route.params;
   const enemies = mission.enemies;
-  const mercenaries = selectedMercenaries;
   let counter = 0;
 
-  console.log('mission', mission);
-  console.log('enemies', enemies);
-  console.log('mercenaries', mercenaries);
+// Convert mercenaries object to an array with type annotation
+const mercenaryArray = Object.keys(selectedMercenaries).map(key => ({
+  type: 'mercenary',
+  name: selectedMercenaries[key]
+}));
 
-  // incluir mercenarios en la lista de enemigos y ordenar por iniciativa que va a ser random
+// Convert enemies array to include a type property
+const typedEnemies = enemies.map(enemy => ({
+  ...enemy,
+  type: 'enemy'
+}));
+
+// Combine both arrays into one
+const combinedArray = [...typedEnemies, ...mercenaryArray];
+
+// Output the combined array to console
+console.log(combinedArray);
 
   return (
     <ScrollView>
       <Text style={style.clock}>tiempo atras</Text>
-      {enemies.map((enemy, index) => (
-        counter = 0,
-        Array.from({ length: enemy.quantity }).map((_, i) => (
-          counter+=1,
-          <EnemyInitiative key={`${index}-${i}`} id={counter} enemy={enemy} />
-        ))
-      ))}
+      {combinedArray.map((character, index) => {
+        if (character.type === 'enemy') {
+          return Array.from({ length: character.quantity }).map((_, i) => {
+            return <EnemyInitiative key={`${index}-${i}`} id={i + 1} enemy={character} />;
+          });
+        } else {
+          return <Text key={index}>{character.name}</Text>;
+        }
+      })}
       <Text>Objective:</Text>
     </ScrollView>
   )
