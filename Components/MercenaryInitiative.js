d0a04a35c2bbc6ec5ae5f816f0ee30bea27b02b8
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2024-05-08 17:45:17 -0500

    include logic to count hp and delete mercenaries

--- Components/MercenaryInitiative.js
+++ Components/MercenaryInitiative.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const MercenaryInitiative = ({ listNum, mercenary }) => {
+const MercenaryInitiative = ({ listNum, mercenary, onMercenaryDelete }) => {
+  const [mercenaryHp, setMercenaryHp] = useState(mercenary.hp);
   return (
     <View style={styles.card}>
       <Text>Initiative: {listNum}</Text>
       <Text style={styles.name}>{mercenary.name}</Text>
+      <Text>HP: {mercenaryHp}</Text>
+      <TouchableOpacity 
+        disabled={mercenaryHp <= 0} 
+        style={[styles.damageButtons, mercenaryHp <= 0 && styles.disabledButton]} 
+        onPress={() => setMercenaryHp(mercenaryHp - 1)}>
+        <Text>Damage</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.cureButtons} onPress={() => setMercenaryHp(mercenaryHp + 1)}>
+        <Text>Cure</Text>
+      </TouchableOpacity>
+      {mercenaryHp <= 0 && <TouchableOpacity style={styles.deleteButtons} onPress={() => onMercenaryDelete(mercenary)}>
+        <Text>Remove Hero</Text>
+      </TouchableOpacity>}
     </View>
   );
 };
@@ -39,6 +54,10 @@ const styles = StyleSheet.create({
     width: 50,
     opacity: 0.5,
   },
+  disabledButton: {
+    opacity: 0.3,
+    backgroundColor: 'grey',  // This can be adjusted to suit your design preferences
+  },
   cureButtons: {
     backgroundColor: '#39FF14',
     marginHorizontal: 20,
@@ -48,6 +67,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     height: 50,
     width: 50,
+    opacity: 0.5,
+  },
+  deleteButtons: {
+    backgroundColor: 'black',
+    marginHorizontal: 20,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: 200,
     opacity: 0.5,
   },
 });
